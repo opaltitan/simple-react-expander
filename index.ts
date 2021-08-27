ec9eb60caea37072c62b79de683f036cb5c0aa77
Justin Patterson <patte2jd@gmail.com>
2021-08-27 14:56:52 -0400

    Moved index file

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { ExpanderParams } from './src/models';
-import { ExpanderComponent } from './src/Expander';
-
-export namespace Expander {
-  export const Expander = ExpanderComponent;
-
-  export interface Params extends ExpanderParams { }
-};
